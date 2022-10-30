54e18538a961ccab13d6a454dd042900b07b9dd9
patrick <corkscrewhollow@gmail.com>
2022-10-30 12:08:36 -0400

    trying to merge

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -15,7 +15,7 @@ router.get("/api/csrf/restore", (req, res) => {
 
 const apiRouter = require('./api');
 
-// router.use('/api', apiRouter);
+router.use('/api', apiRouter);
 // ...
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
