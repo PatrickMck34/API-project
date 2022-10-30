b6fc1a099eb2c134a32cd7f2d3b10dd0f6138014
patrick <corkscrewhollow@gmail.com>
2022-10-30 16:38:32 -0400

    Spot function added

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
               const spot = await Spot.create({
                 address, city, state, country, lat, lng, name, description, price
               });
-              return spot
+              return await Spot.findByPk(spot.id);
             }
      static getSpots() {
       const spot = Spot.findAll({})
