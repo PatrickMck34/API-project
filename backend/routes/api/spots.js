b3845c40d7562a81d02ba17db390f3898f6528fd
patrick <corkscrewhollow@gmail.com>
2022-10-31 03:17:17 -0400

    reset DB

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const { Spot } = require('../../db/models');
 const router = express.Router();
-
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
 // backend/routes/api/session.js
 
 
+router.get(
+    '/current', restoreUser, async (req, res) => {
+        let  {user} = req;
+         const ids = user.id
+         
+        const spots = await Spot.findByPk({ids          
+        })
+            
+   
+            return res.json({
+            spots,
+            });
+    });
 router.get(
     '/', async (req, res) => {
         const {id, ownersId, address, city, state, country, lat, lng, name, description, price} = req.query
@@ -18,16 +31,17 @@ router.get(
         });
         
         router.get(
-            '/spotsId', async (req, res) => {
-                const spotId = req.params.id;
-                const{id, ownersId, address, city, state, country, lat, lng, name, description, price} = req.query
-                const spots = await Spot.findByPk()
+            '/current', async (req, res) => {
+                const {id, ownersId, address, city, state, country, lat, lng, name, description, price} = req.query
+                const spots = await Spot.findAll({
+                    id, ownersId, address, city, state, country, lat, lng, name, description, price})
                     
-           
+                    
                     return res.json({
-                    spots,
+                        spots,
                     });
-            });
+                });
+                
         router.post("/", async (req, res) => {
             const { id, ownerId, address, city, state, country, lat, lng, name, description, price} = req.body;
             const spot = await Spot.create({id, ownerId, address, city, state, country, lat, lng, name, description, price})
