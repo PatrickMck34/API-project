4efa0c764bba218cf0a05bd3971049ec9837d076
patrick <corkscrewhollow@gmail.com>
2022-11-20 03:47:35 -0500

    fixed user seeder

--- backend/db/seeders/20221021155014-demo-user.js
+++ backend/db/seeders/20221021155014-demo-user.js
@@ -5,16 +5,22 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
+        firstName: "bobd",
+        lastName: 'some13',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: "bob",
+        lastName: 'some12',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: "bobj",
+        lastName: 'some1',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
