1eda1a3165bb1a9107c8d01b4a2272cf73d2eae0
patrick <corkscrewhollow@gmail.com>
2022-11-28 12:39:56 -0500

    reviews Reviews

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -166,9 +166,11 @@ router.get(
                 
                 const check = await Reviews.findByPk(spotId)
                 if (check === null) {
-                    const Reviews = await Reviews.create({ userId, spotId, review, stars })
+                    const reviews = await Reviews.create({ userId, spotId, review, stars })
+                    if (reviews){
+                       const Reviews = reviews
                     return res.json(Reviews).statusCode(201)
-                }
+                }}
                 
                 return res.status(404).json({ message: "Already submitted a review!", statusCode: 404 })
             })
