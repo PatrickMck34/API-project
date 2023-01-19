b1b464c4480f8e2921f309e206349d7e34e06e3d
patrick <corkscrewhollow@gmail.com>
2023-01-19 04:52:22 -0500

    review Card

--- frontend/src/components/ReviewCard/ReviewCards.js
+++ frontend/src/components/ReviewCard/ReviewCards.js
@@ -1,47 +1,62 @@
 import { csrfFetch } from '../../store/csrf'
 import React from 'react'
 import {useSelector, useDispatch} from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useHistory } from 'react-router-dom'
 import * as reviewsActions from "../../store/reviews"
-import { useEffect} from 'react'
+import { useEffect, useState} from 'react'
 import "./ReviewCard.css"
 import DeleteReview from '../DeleteReview/DeleteReviews'
 import OpenModalButton from '../OpenModalButton'
+import * as sessionActions from "../../store/session"
 function ReviewCard() {
+    const history = useHistory()
     const dispatch=useDispatch()
-    const {spotsId} = {useParams}
+    const {spotsId} = useParams()
     const reviewsObj = useSelector(state=> state.reviews.allReviews)
+    const user = useSelector(state => state.session.user.id)
     const reviews = Object.values(reviewsObj) 
 
  useEffect(() => {
    if(reviews)
      dispatch(reviewsActions.getReviews(spotsId))
-  
-    
    }
 , [dispatch])
+let revId
+
+const DeleteReview = () => {
+   
+     dispatch(reviewsActions.deleteReviews(revId))
+     history.push(`/`)
+}
 
  return(
      
      reviews.map((review) => {
-         
+         revId = review.id
              return(
                  <>
       
 
 
 <div key={review.id} className="ReviewPad">
-
+        {review.User.firstName} says: <p></p>
         {review.review} 
 
         </div>
+<p></p>
+        <div>
+      
+            {(review.User.id === user) ? (
 
-        {/* <div>
-        <button onclick={(dispatch(reviewsActions.deleteReviews(review.id)),[dispatch])}>
+                <button className="button" onClick={()=>DeleteReview(review.id)}>
+            Delete Review
 
         </button>
-        </div> */}
-           </>
+            ):(
+                <div></div>
+                )}
+                </div>
+                </>
          
        
          
